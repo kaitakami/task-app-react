f590dd54f523d9969df69f845d309099524878fc
Kai Takami <kaitakami@protonmail.com>
2022-08-27 16:29:06 +0900

    Implement edit and delete function for each task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,103 @@
 import { Component } from "react";
-
+import Overview from "./components/Overview";
+import uniqid from "uniqid";
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      task: {
+        name: "",
+        id: uniqid(),
+      },
+      tasks: [],
+      edit: false,
+    };
+  }
+
+  handleRemove = (deletedTask) => {
+    // eslint-disable-next-line no-restricted-globals
+    const remove = confirm("Do you want to remove this task?");
+    if (remove) {
+      const updatedArr = this.state.tasks.filter(
+        (task) => task.id != deletedTask.id
+      );
+      this.setState({
+        tasks: [...updatedArr],
+      });
+    }
+  };
+
+  handleEdit = (editedTask) => {
+    this.setState({
+      task: {
+        name: editedTask.name,
+        id: editedTask.id,
+      },
+      edit: true,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.edit) {
+      const updatedArr = this.state.tasks.map((task) =>
+        task.id === this.state.task.id ? this.state.task : task
+      );
+      this.setState({
+        tasks: [...updatedArr],
+      });
+    } else {
+      this.setState({
+        tasks: [...this.state.tasks, this.state.task],
+      });
+    }
+    this.setState({
+      task: {
+        name: "",
+        id: uniqid(),
+      },
+      edit: false,
+    });
+  };
+
   render() {
-    return <div className="App"></div>;
+    const { edit } = this.state;
+    return (
+      <div className="App">
+        <div className="container">
+          <form onSubmit={this.handleSubmit} className="form">
+            <label htmlFor="task" className="label">
+              {edit ? "Edit your task" : "Write your task"}
+            </label>
+            <div className="inputs">
+              <input
+                id="task"
+                className="input"
+                type="text"
+                minLength="2"
+                value={this.state.task.name}
+                onChange={(e) =>
+                  this.setState({
+                    task: {
+                      name: `${e.target.value}`,
+                      id: this.state.task.id,
+                    },
+                  })
+                }
+                placeholder="ex) Clean the house"
+                required
+              />
+              <input type="submit" value={edit ? "Edit" : "Add"} className="button-27" />
+            </div>
+          </form>
+          <Overview
+            tasks={this.state.tasks}
+            handleRemove={this.handleRemove}
+            handleEdit={this.handleEdit}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/components/Overview.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import Task from "./Task";
+
+class Overview extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        {this.props.tasks.length ? (
+          <>
+            <h2>Your tasks</h2>
+            <div className="tasks">
+              {this.props.tasks.map((task) => {
+                return <Task key={task.id} task={task} tasks={this.props.tasks} handleRemove={this.props.handleRemove} handleEdit={this.props.handleEdit} >{task.name}</Task>;
+              })}
+            </div>
+          </>
+        ) : (
+          <h2>Start adding a task</h2>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Overview;
